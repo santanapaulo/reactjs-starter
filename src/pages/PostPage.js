0d17a4381aca5087a8b471b3e54975f2b49dcf9c
Paulo <pauloguedescamargo@gmail.com>
2016-10-05 22:44:01 -0300

    post page

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,8 +1,13 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/raisedButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import AccountCircle from 'material-ui/svg-icons/action/account-circle';
+import AccessTime from 'material-ui/svg-icons/device/access-time';
+import Save from 'material-ui/svg-icons/content/save';
+import Create from 'material-ui/svg-icons/content/create';
+import IconButton from 'material-ui/IconButton';
 
 const getStyles = () => {
   const styles = {
@@ -22,43 +27,74 @@ const getStyles = () => {
   return styles;
 };
 
-const PostPage = (props) => {
-  const {
-    postList,
-    zDepth,
-    paperStyle,
-    pageStyle,
-  } = props;
+class PostPage extends Component {
 
-  const styles = getStyles();
+  constructor(props){
+    super(props);
+    this.state = {
+      isEditing: false,
+    };
+  }
 
-  const getPost = () => {
-    console.log(postList);
-    const posts = postList.map((post) => (
-        <Paper style={Object.assign(styles.paper, paperStyle)} zDepth={zDepth}>
-          <div className="header">
-            <div className="fields">
-              {post.name}
+  handleClick() {
+    console.log('ok');
+    console.log(this.state);
+    this.setState({
+      isEditing: !this.state.isEditing,
+    });
+  }
+
+  render() {
+
+    const {
+      postList,
+      zDepth,
+      paperStyle,
+      pageStyle,
+    } = this.props;
+
+    const styles = getStyles();
+
+    const getPost = () => {
+      const icon = this.state.isEditing ?
+        <Save />
+      :
+        <Create />;
+      const posts = postList.map((post) => (
+          <Paper style={Object.assign(styles.paper, paperStyle)} zDepth={zDepth}>
+            <div className="header">
+              <div className="fields">
+                <AccountCircle />
+                {post.name}
+              </div>
+              <div className="fields">
+                <AccessTime />
+                {post.time}
+              </div>
+              <div className="fields">
+                <IconButton
+                  onClick={this.handleClick}
+                >
+                  {icon}
+                </IconButton>
+              </div>
             </div>
-            <div className="fields">
-              {post.time}
+            <div className="comment-part">
+              {post.comment}
             </div>
-          </div>
-          <div className="comment-part">
-            {post.comment}
-          </div>
-        </Paper>
+          </Paper>
+        )
       )
-    )
-    return posts;
-  };
+      return posts;
+    };
 
-  return (
-    <div className="post-page" style={Object.assign(styles.page, pageStyle)}>
-    {console.log(getPost())}
-      {getPost()}
-    </div>
-  );
+    
+    return (
+      <div className="post-page" style={Object.assign(styles.page, pageStyle)}>
+        {getPost()}
+      </div>
+    );
+  }
 };
 
 PostPage.propTypes = {
